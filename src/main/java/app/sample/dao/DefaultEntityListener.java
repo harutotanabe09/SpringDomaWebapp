7df8a3834072a34957054396bf6065a77401567c
Haruto Tanabe <harutotanabe@HarutonoMacBook-Air.local>
2019-07-21 12:25:28 +0900

    login userName

--- src/main/java/app/sample/dao/DefaultEntityListener.java
+++ src/main/java/app/sample/dao/DefaultEntityListener.java
@@ -1,14 +1,26 @@
 package app.sample.dao;
 
-import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.time.LocalDateTime;
+
 import org.seasar.doma.jdbc.entity.EntityListener;
 import org.seasar.doma.jdbc.entity.PreInsertContext;
+
+import app.sample.entity.DomaDtoImpl;
+import app.sample.util.WebSecurityUtils;
+import lombok.NoArgsConstructor;
+import lombok.val;
+import lombok.extern.slf4j.Slf4j;
 
 @NoArgsConstructor // コンストラクタが必須のため
 @Slf4j
 public class DefaultEntityListener<ENTITY> implements EntityListener<ENTITY> {
 
   @Override
-  public void preInsert(ENTITY entity, PreInsertContext<ENTITY> context) {}
+  public void preInsert(ENTITY entity, PreInsertContext<ENTITY> context) {
+	  val domaDto = (DomaDtoImpl) entity;
+      val createdAt = LocalDateTime.now();
+      val createdBy = WebSecurityUtils.getLoginId();
+      domaDto.setCreatedAt(createdAt);
+      domaDto.setCreatedBy(createdBy);
+  }
 }
